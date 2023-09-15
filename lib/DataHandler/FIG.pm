aaf09976400086e971e7dc30c8b54aba97159a04
Robert Olson <olson@mcs.anl.gov>
2023-09-15 14:42:52 -0500

    use the already-created FIG for the new FIGM

--- lib/DataHandler/FIG.pm
+++ lib/DataHandler/FIG.pm
@@ -183,7 +183,7 @@ sub handle {
       # print STDERR "FIGM: " . Dumper(\@fig_m_dirs);
       if (scalar(@fig_m_dirs)) {
 	# create the FIGM
-	$self->{_figm} = FIGM->new(undef, @fig_m_dirs);
+	$self->{_figm} = FIGM->new($self->{_fig}, @fig_m_dirs);
 	
 	return $self->{_figm}
       }
