1551f8662949870dc44813894168a59bdb5f5bb4
Robert Olson <olson@mcs.anl.gov>
2023-09-15 11:23:13 -0500

    Debg

--- lib/WebApplication.pm
+++ lib/WebApplication.pm
@@ -240,11 +240,11 @@ sub new {
   
   return $self;
 }
-  
+
   sub DESTROY
   {
       my($self) = @_;
-      print STDERR "DESTROY webapp $self\n";
+      print STDERR "DESTROY webapp $self $$\n";
   }
 
 
